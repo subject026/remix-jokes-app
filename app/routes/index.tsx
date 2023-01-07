054d30cde0568d5fe6e4572fe378b23d2df7bbdd
subject026 <subject026@protonmail.com>
2023-01-07 22:04:51 +0000

    init project setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export default function IndexRoute() {
+  return <h1>Hello index.tsx</h1>;
+}
