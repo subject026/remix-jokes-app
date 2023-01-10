3b6e554ac39beb91b84758569993255222eb0989
subject026 <subject026@protonmail.com>
2023-01-10 10:48:14 +0000

    display logged in user and create logout route

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,17 +1,26 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
+import { getUser } from "../utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderArgs) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
   return json({
-    jokeListItems: await db.joke.findMany(),
+    jokeListItems,
+    user,
   });
 };
 
@@ -28,6 +37,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
